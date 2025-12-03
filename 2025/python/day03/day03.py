cfc26c095af77e5dc1aeba902f72354b65cdc902
Andrew Lau <82020841+BalloonAnd888@users.noreply.github.com>
2025-12-03 00:33:42 -0800

    day 03

---
+++ 2025/python/day03/day03.py
@@ -0,0 +1,55 @@
+def p1():
+    inputs = []
+    with open("input.txt", 'r') as file:
+        for line in file:
+            inputs.append(list(map(int, line.strip())))
+    
+    print(inputs)
+    sum = 0
+
+    for x in inputs:
+        temp = 0
+
+        for i in range(len(x)):
+            for j in range(i + 1, len(x)):
+                val = int(str(x[i])+str(x[j]))
+                if val > temp:
+                    temp = val
+
+        sum += temp
+
+    print("Part 1 answer:", sum)
+
+def p2():
+    inputs = []
+    with open("input.txt", 'r') as file:
+        for line in file:
+            inputs.append(list(map(int, line.strip())))
+    
+    print(inputs)
+    sum = 0
+
+    for x in inputs:
+        l = 0
+        temp = ""
+        window = 12 
+
+        while window > 0:
+            diff = window - 1
+            end = len(x) - diff
+            search = x[l : end]
+            best = max(search)
+            index = search.index(best)
+            temp += str(best)
+            
+            l += index + 1
+            window -= 1
+        
+        sum += int(temp)
+
+    print("Part 2 answer:", sum)
+
+if __name__ == '__main__':
+    p1()
+    p2()
+    
